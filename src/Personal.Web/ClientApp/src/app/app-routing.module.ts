cb20f7cfda6cd5c21816553224454681c11908ac
Tomasz Grabusiński <tomasz@grabusinski.pl>
2022-11-22 23:55:36 +0100

    Add modules: - about - portfolio - blog

--- src/Personal.Web/ClientApp/src/app/app-routing.module.ts
+++ src/Personal.Web/ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', loadChildren: () => import('./modules/about/about.module').then(m => m.AboutModule) },
+  { path: 'blog', loadChildren: () => import('./modules/blog/blog.module').then(m => m.BlogModule) },
+  { path: 'portfolio', loadChildren: () => import('./modules/portfolio/portfolio.module').then(m => m.PortfolioModule) },
+  { path: '', redirectTo: '/about', pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
