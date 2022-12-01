95ef5041152279dbcbcbf369f82e85bb7d80f01a
Tomasz Grabusiński <tgrabus@outlook.com>
2022-12-01 23:19:31 +0100

    Add home page (#2)

--- src/Personal.Web/ClientApp/src/app/app-routing.module.ts
+++ src/Personal.Web/ClientApp/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) },
   { path: 'about', loadChildren: () => import('./modules/about/about.module').then(m => m.AboutModule) },
   { path: 'blog', loadChildren: () => import('./modules/blog/blog.module').then(m => m.BlogModule) },
   { path: 'portfolio', loadChildren: () => import('./modules/portfolio/portfolio.module').then(m => m.PortfolioModule) },
-  { path: '', redirectTo: '/about', pathMatch: "full"}
+  { path: '', redirectTo: '/home', pathMatch: "full"},
+
 ];
 
 @NgModule({
