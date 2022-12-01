7e31b7d3093551027a3ab0712b9fba053e0654c1
Tomasz Grabusiński <tomasz@grabusinski.pl>
2022-12-01 23:46:04 +0100

    Revert "Fix sonar issues (#3)"
    This reverts commit 418215189cf8d1744e10ee2a1ddf0af3c23fff71.

--- src/Personal.Web/ClientApp/src/app/modules/blog/blog.component.ts
+++ src/Personal.Web/ClientApp/src/app/modules/blog/blog.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.scss']
 })
-export class BlogComponent  {
+export class BlogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
--- src/Personal.Web/ClientApp/src/app/modules/portfolio/portfolio.component.ts
+++ src/Personal.Web/ClientApp/src/app/modules/portfolio/portfolio.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss']
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
